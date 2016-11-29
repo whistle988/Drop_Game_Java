f8605b4a145e1a2b4930edad7d68de5381ca961c
whistle988 <chageshii@gmail.com>
2016-11-29 23:11:33 +0900

    First Commit

---
+++ core/src/com/chageshii/game/Drop.java
@@ -0,0 +1,32 @@
+package com.chageshii.game;
+
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Drop extends Game{
+
+    SpriteBatch batch;
+    BitmapFont font;
+
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        font = new BitmapFont();
+        this.setScreen(new MainMenuScreen(this));
+    }
+
+    @Override
+    public void render() {
+        super.render();
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        batch.dispose();
+        font.dispose();
+    }
+}
